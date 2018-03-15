1270cd3955b36e1f5003cba3edcfbf17c59d3c85
LAB <LAB>
2018-03-15 14:58:28 -0500

    work

---
+++ Hayden/Webb_Assignment5/Ch5-Lecture-PtB/Ch5-Lecture/Form1.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ch5Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.averageLabel = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CalcToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StyleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisplayLabelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.FontToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.titleLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.game2TextBox = New System.Windows.Forms.TextBox()
+        Me.game3TextBox = New System.Windows.Forms.TextBox()
+        Me.game1TextBox = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.highGameLabel = New System.Windows.Forms.Label()
+        Me.handicapLabel = New System.Windows.Forms.Label()
+        Me.seriesLabel = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'averageLabel
+        '
+        Me.averageLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.averageLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.averageLabel.Location = New System.Drawing.Point(310, 99)
+        Me.averageLabel.Name = "averageLabel"
+        Me.averageLabel.Size = New System.Drawing.Size(88, 20)
+        Me.averageLabel.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.CalcToolStripMenuItem1, Me.ClearToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(536, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(92, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'CalcToolStripMenuItem1
+        '
+        Me.CalcToolStripMenuItem1.Name = "CalcToolStripMenuItem1"
+        Me.CalcToolStripMenuItem1.Size = New System.Drawing.Size(42, 20)
+        Me.CalcToolStripMenuItem1.Text = "&Calc"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StyleToolStripMenuItem, Me.DisplayLabelToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'StyleToolStripMenuItem
+        '
+        Me.StyleToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontToolStripMenuItem, Me.ToolStripSeparator1, Me.ColorToolStripMenuItem})
+        Me.StyleToolStripMenuItem.Name = "StyleToolStripMenuItem"
+        Me.StyleToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.StyleToolStripMenuItem.Text = "Style"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.FontToolStripMenuItem.Text = "Font"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(138, 6)
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
+        Me.ColorToolStripMenuItem.Text = "Color--Input"
+        '
+        'DisplayLabelToolStripMenuItem
+        '
+        Me.DisplayLabelToolStripMenuItem.Checked = True
+        Me.DisplayLabelToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.DisplayLabelToolStripMenuItem.Name = "DisplayLabelToolStripMenuItem"
+        Me.DisplayLabelToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.DisplayLabelToolStripMenuItem.Text = "Display Label"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FontToolStripMenuItem1, Me.ColorToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(152, 48)
+        '
+        'FontToolStripMenuItem1
+        '
+        Me.FontToolStripMenuItem1.Name = "FontToolStripMenuItem1"
+        Me.FontToolStripMenuItem1.Size = New System.Drawing.Size(151, 22)
+        Me.FontToolStripMenuItem1.Text = "Font"
+        '
+        'ColorToolStripMenuItem1
+        '
+        Me.ColorToolStripMenuItem1.Name = "ColorToolStripMenuItem1"
+        Me.ColorToolStripMenuItem1.Size = New System.Drawing.Size(151, 22)
+        Me.ColorToolStripMenuItem1.Text = "Color--Output"
+        '
+        'titleLabel
+        '
+        Me.titleLabel.AutoSize = True
+        Me.titleLabel.Font = New System.Drawing.Font("Monotype Corsiva", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.titleLabel.Location = New System.Drawing.Point(154, 40)
+        Me.titleLabel.Name = "titleLabel"
+        Me.titleLabel.Size = New System.Drawing.Size(193, 36)
+        Me.titleLabel.TabIndex = 5
+        Me.titleLabel.Text = "Bowl-A-RAMA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Game 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Game 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 165)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Game 3"
+        '
+        'game2TextBox
+        '
+        Me.game2TextBox.Location = New System.Drawing.Point(94, 133)
+        Me.game2TextBox.Name = "game2TextBox"
+        Me.game2TextBox.Size = New System.Drawing.Size(61, 20)
+        Me.game2TextBox.TabIndex = 1
+        '
+        'game3TextBox
+        '
+        Me.game3TextBox.Location = New System.Drawing.Point(94, 162)
+        Me.game3TextBox.Name = "game3TextBox"
+        Me.game3TextBox.Size = New System.Drawing.Size(61, 20)
+        Me.game3TextBox.TabIndex = 2
+        '
+        'game1TextBox
+        '
+        Me.game1TextBox.Location = New System.Drawing.Point(94, 99)
+        Me.game1TextBox.Name = "game1TextBox"
+        Me.game1TextBox.Size = New System.Drawing.Size(61, 20)
+        Me.game1TextBox.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(245, 99)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Average"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(245, 132)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Series"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(245, 171)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(53, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Handicap"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(245, 212)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(60, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "High Game"
+        '
+        'highGameLabel
+        '
+        Me.highGameLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.highGameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.highGameLabel.Location = New System.Drawing.Point(310, 206)
+        Me.highGameLabel.Name = "highGameLabel"
+        Me.highGameLabel.Size = New System.Drawing.Size(88, 20)
+        Me.highGameLabel.TabIndex = 6
+        '
+        'handicapLabel
+        '
+        Me.handicapLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.handicapLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.handicapLabel.Location = New System.Drawing.Point(311, 165)
+        Me.handicapLabel.Name = "handicapLabel"
+        Me.handicapLabel.Size = New System.Drawing.Size(88, 20)
+        Me.handicapLabel.TabIndex = 5
+        '
+        'seriesLabel
+        '
+        Me.seriesLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.seriesLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.seriesLabel.Location = New System.Drawing.Point(310, 132)
+        Me.seriesLabel.Name = "seriesLabel"
+        Me.seriesLabel.Size = New System.Drawing.Size(88, 20)
+        Me.seriesLabel.TabIndex = 4
+        '
+        'ch5Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(536, 449)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.seriesLabel)
+        Me.Controls.Add(Me.handicapLabel)
+        Me.Controls.Add(Me.highGameLabel)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.game1TextBox)
+        Me.Controls.Add(Me.game3TextBox)
+        Me.Controls.Add(Me.game2TextBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.titleLabel)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.averageLabel)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "ch5Form"
+        Me.Text = "Menus"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents averageLabel As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StyleToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DisplayLabelToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents FontDialog1 As System.Windows.Forms.FontDialog
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents FontToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents titleLabel As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents game2TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents game3TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents game1TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents highGameLabel As System.Windows.Forms.Label
+    Friend WithEvents handicapLabel As System.Windows.Forms.Label
+    Friend WithEvents seriesLabel As System.Windows.Forms.Label
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CalcToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Hayden/Webb_Assignment5/Ch5-Lecture-PtB/Ch5-Lecture/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class ch5Form
+
+    Private Sub DisplayLabelToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DisplayLabelToolStripMenuItem.Click
+        If DisplayLabelToolStripMenuItem.Checked Then
+            'There is a checkmark,  remove it
+            DisplayLabelToolStripMenuItem.Checked = False
+            titleLabel.Visible = False
+        Else
+            'There is not a checkmark, put it there
+            DisplayLabelToolStripMenuItem.Checked = True
+            titleLabel.Visible = True
+        End If
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FontToolStripMenuItem.Click, FontToolStripMenuItem1.Click
+        With FontDialog1
+            .Font = averageLabel.Font
+            .ShowDialog()
+            averageLabel.Font = .Font
+            seriesLabel.Font = .Font
+            handicapLabel.Font = .Font
+            highGameLabel.Font = .Font
+            game1TextBox.Font = .Font
+            game2TextBox.Font = .Font
+            game3TextBox.Font = .Font
+            game1TextBox.Focus()
+        End With
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ColorToolStripMenuItem.Click
+        Dim inputColor As Color
+        inputColor = game1TextBox.ForeColor
+        With ColorDialog1
+            'Call a procedure named SelectColor 
+            SelectColor(inputColor)
+            game1TextBox.ForeColor = .Color
+            game2TextBox.ForeColor = .Color
+            game3TextBox.ForeColor = .Color
+        End With
+    End Sub
+
+    Private Sub ColorToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ColorToolStripMenuItem1.Click
+        Dim originalColor As Color
+        originalColor = averageLabel.ForeColor
+        With ColorDialog1
+            'Call a procedure named SelectColor
+            SelectColor(originalColor)
+            averageLabel.ForeColor = .Color
+            seriesLabel.ForeColor = .Color
+            handicapLabel.ForeColor = .Color
+            highGameLabel.ForeColor = .Color
+        End With
+    End Sub
+    'Create a procedure named SelectColor whose purpose is to pop up the color dialog box
+    Private Sub SelectColor(incomingColor As Color)
+        With ColorDialog1
+            .Color = incomingColor
+            .ShowDialog()
+        End With
+    End Sub
+
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+  
+    Private Sub CalcToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CalcToolStripMenuItem1.Click
+        Dim AverageDecimal, HandicapDecimal As Decimal
+        Dim SeriesInteger, Game1Integer, Game2Integer, Game3Integer As Integer
+        Dim HighGameString As String = ""
+
+        Try
+            Game1Integer = Integer.Parse(game1TextBox.Text)
+            Game2Integer = Integer.Parse(game2TextBox.Text)
+            Game3Integer = Integer.Parse(game3TextBox.Text)
+            'Perform all calculations by calling functions
+            AverageDecimal = FindAverage(Game1Integer, Game2Integer, Game3Integer)
+            SeriesInteger = FindSeries(Game1Integer, Game2Integer, Game3Integer)
+            HighGameString = FindHighGame(Game1Integer, Game2Integer, Game3Integer)
+            HandicapDecimal = FindHandicap(Game1Integer, Game2Integer, Game3Integer)
+
+            'Display formatted output
+            averageLabel.Text = AverageDecimal.ToString("N1")
+            highGameLabel.Text = HighGameString
+            seriesLabel.Text = SeriesInteger.ToString("N0")
+            handicapLabel.Text = HandicapDecimal.ToString("N1")
+
+        Catch ex As FormatException
+            MessageBox.Show("Please enter three numeric scores", "Missing Data", MessageBoxButtons.OK)
+        End Try
+    End Sub
+    'Create a function named FindAverage which will return the average of the three games scores
+    Private Function FindAverage(score1 As Integer, score2 As Integer, score3 As Integer) As Decimal
+        Return ((score1 + score2 + score3) / 3)
+    End Function
+    'Create a function named FindSeries which will return the sum of the three game scores
+    Private Function FindSeries(score1 As Integer, score2 As Integer, score3 As Integer) As Integer
+        Return (score1 + score2 + score3)
+    End Function
+    'Create a function named FindHighGame which will return the game number as a string of the highest game in the series
+    Private Function FindHighGame(game1 As Integer, game2 As Integer, game3 As Integer) As String
+        If game1 > game2 And game1 > game3 Then
+            Return "1"
+        ElseIf game2 > game1 And game2 > game3 Then
+            Return "2"
+        ElseIf game3 > game1 And game3 > game2 Then
+            Return "3"
+        Else
+            Return "Tie"
+        End If
+    End Function
+    'Create a function named FindHandicap which will return a bowler's handicap.  
+    Private Function FindHandicap(game1 As Integer, game2 As Integer, game3 As Integer) As Decimal
+
+        'The handicap is calculated by subtracting the bowler's average from 200 and multiplying by .8
+        Return ((200 - FindAverage(game1, game2, game3)) * 0.8)
+    End Function
+
+    Private Sub ClearToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearToolStripMenuItem.Click
+        game1TextBox.Clear()
+        game2TextBox.Clear()
+        game3TextBox.Clear()
+        averageLabel.Text = String.Empty
+        highGameLabel.Text = String.Empty
+        seriesLabel.Text = String.Empty
+        handicapLabel.Text = String.Empty
+    End Sub
+
+End Class
